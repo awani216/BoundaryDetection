09259516d185090a4c0905406ec8cf86ba28c6ec
awani216 <awani.mishra216@gmail.com>
2018-05-21 15:47:46 +0530

    caption detection improved

---
+++ codes/textualAnalysis/captions_search.py
@@ -0,0 +1,21 @@
+import re 
+
+def find_captions():
+        datafile = file(r'/home/walter-white/Desktop/test/files_used/textualAnalysis/subtitles.txt3')
+        f = open(r'/home/walter-white/Desktop/test/files_generated/textualAnalysis/captions_intervals.txt','w')
+        found = 0
+        ignorecase = re.compile('captions', re.IGNORECASE)
+        for line in datafile:
+            if ignorecase.search(line) : 
+                found = found + 1
+                time = str(3600*int(line[8:10])+60*int(line[10:12])+1*int(line[12:14])+10)+'-'+str(3600*int(line[27:29])+60*int(line[29:31])+int(line[31:33])-10)
+                f.write(time+'\n')
+                print(line)
+                print(time)
+
+               
+        f.close()
+        return found 
+
+
+print find_captions()
