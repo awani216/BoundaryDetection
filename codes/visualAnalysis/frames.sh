8d75300c452d0ea5502a589c41090d819c198c60
awani216 <awani.mishra216@gmail.com>
2018-05-31 16:17:28 +0530

    started the visualAnalysis

--- codes/visualAnalysis/frames.sh
+++ codes/visualAnalysis/frames.sh
@@ -1,16 +1,14 @@
 #this file is for dividing videos into frames.
-
 #!/bin/bash
 
 $cmd=""
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo $line
     IFS='-' read -ra tmp<<<"$line"
-    l1=`expr ${tmp[0]} - 20`
-    l2=`expr ${tmp[1]} - 20`
+    l1=`expr ${tmp[1]} - 20`
+    l2=`expr ${tmp[0]} - ${tmp[1]} + 40`
     echo ${l1}
-    cmd="$cmd && sudo ffmpeg -ss ${l1} -i v2.mp4 -t 40 -vf fps=1 generated2/${l1}_%d.jpg" 
-    cmd="$cmd && sudo ffmpeg -ss ${l2} -i v2.mp4 -t 40 -vf fps=1 generated2/${l2}_%d.jpg"  
+    cmd="$cmd && sudo ffmpeg -ss ${l1} -i {$2} -t ${l2} -vf fps=1 {$3}/${l1}_%d.jpg"   
 done < "$1"
 cmd=${cmd:3}
 echo $cmd
