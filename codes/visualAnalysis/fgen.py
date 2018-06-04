de46c7a58a9f8634207881503276176ae3a1a71b
awani216 <awani.mishra216@gmail.com>
2018-06-04 14:43:58 +0530

    visualAnalysis updated

--- codes/visualAnalysis/fgen.py
+++ codes/visualAnalysis/fgen.py
@@ -4,7 +4,7 @@ def createFrames(dirname):
     if os.isdir(dirname):
         dirlist = os.listdir(dirname)
         dname = os.path.abspath("../../files_generated/audioAnalysis/music_intervals")
-        relname = dirlist.strip(dname)
+        relname = dirname.strip(dname)
         rootname =  r'/home/awanimishra/space/BoundaryDetection/files_generated/visualAnalysis/music_intervals'
         absname = rootname + relname
         os.makedirs(absname)
@@ -12,11 +12,13 @@ def createFrames(dirname):
             if(dirc.endswith('.txt')):
                 f.open((dirname + "/" + dirc),'r')
                 for line in f:
+                    basenm = os.path.splitext(dirc)[0]
                     videopath = os.path.abspath("../../../../Rosenthal/")
-                    videopath = videopath + relname.strip(".txt") + ".mp4"
+                    videopath = videopath + relname + "/" + dirc
                     end,start = line.split('-')
-                    os.system("ffmpeg -ss " + start + " -i " + videopath + " -t " + start-end + " -vf fps=1 " + absname+"/"+dirc.strip(".txt")+ "%d.jpg")   
-            elif os.isdir(dirc)
+                    os.system("ffmpeg -ss " + start + " -i " + videopath + " -t " + start-end 
+                    + " -vf fps=1 " + absname+"/"+basenm+ "%d.jpg")   
+            elif os.isdir(dirname + '/' + dirc):
                 createFrames(dirname + '/' + dirc)
        
 
