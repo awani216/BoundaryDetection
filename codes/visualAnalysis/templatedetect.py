fb474e53416856960bbe1ca4babe91ffff753587
awani216 <awani.mishra216@gmail.com>
2018-06-15 14:33:46 +0530

    visualAnalysis updated

--- codes/visualAnalysis/templatedetect.py
+++ codes/visualAnalysis/templatedetect.py
@@ -5,13 +5,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 
+templates = []
+template_files = os.listdir("template")
+for t_file in template_files:
+    template1 = cv2.imread("template/" +  template,-1)
+    template = cv2.cvtColor(template1, cv2.COLOR_BGR2GRAY)  
+    templates.append(template) 
 def find(ip, template_files):
     img_rgb = cv2.imread(ip, -1)
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-    for template in template_files:
-        template1 = cv2.imread("template/" +  template,-1)
-        template = cv2.cvtColor(template1, cv2.COLOR_BGR2GRAY)
-
+    for template in templates:
         w, h = template.shape[::-1]
         res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
         threshold = 0.8
@@ -30,7 +33,6 @@ def find(ip, template_files):
     cv2.imwrite("ellen2/"+ip.split("/")[1],img_rgb)
 
 image_files = os.listdir("generated2")
-template_files = os.listdir("template")
 for image in image_files:
     ip = "generated2/"+image
-    find(ip, template_files)
+    find(ip)
