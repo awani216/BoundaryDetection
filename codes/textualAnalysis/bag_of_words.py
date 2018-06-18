07ce77c5dfe7fb5a58af0706f099cae5825b8cde
awani216 <awani.mishra216@gmail.com>
2018-06-18 16:07:05 +0530

    bag of words model created

--- codes/textualAnalysis/bag_of_words.py
+++ codes/textualAnalysis/bag_of_words.py
@@ -6,7 +6,7 @@ import pickle
 from sklearn.naive_bayes import MultinomialNB 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
 
-f = open(r'/home/walter-white/Desktop/test/files_used/textualAnalysis/V1.txt','r')
+f = open(r'/home/walter-white/Desktop/test/files_used/textualAnalysis/V1.txt','r') 
 corpus = []
 for line in f:
     corpus.append(line)
