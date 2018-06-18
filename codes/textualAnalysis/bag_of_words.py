32b708d309c49f60e57d7b81f31468466e2e5048
awani216 <awani.mishra216@gmail.com>
2018-06-18 15:29:41 +0530

    bag of words model created

--- codes/textualAnalysis/bag_of_words.py
+++ codes/textualAnalysis/bag_of_words.py
@@ -1,2 +1,16 @@
 #bag of words model implementation
+import numpy as np
+import csv
+import sys
+import pickle
+from sklearn.naive_bayes import MultinomialNB 
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
 
+f = open(r'/home/walter-white/Desktop/test/files_used/textualAnalysis/V1.txt','r')
+corpus = []
+for line in f:
+    corpus.append(line)
+
+vectorizer = CountVectorizer()
+print( vectorizer.fit_transform(corpus).todense() )
+print( vectorizer.vocabulary_ )
